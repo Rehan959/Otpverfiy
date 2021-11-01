0404441254ce44f062c5fea5d79e5e4f6edec363
Rehan959 <rehanshami007@gmail.com>
2021-11-01 06:48:02 +0530

    'Otp-verify'

--- routes/index.js
+++ routes/index.js
@@ -1,45 +1,47 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var messagebird = require('messagebird')('TsTUHuDrTkUwHAY56ktunfIB5')
+var messagebird = require("messagebird")("TsTUHuDrTkUwHAY56ktunfIB5");
 
 /* GET home page. */
-router.get('/', function (req, res) {
-res.render('index');
+router.get("/", function (req, res) {
+  res.render("index");
 });
 
-router.post('/step2', (req, res) => {
-var number = req.body.number
+router.post("/step2", (req, res) => {
+  var number = req.body.number;
 
-messagebird.verify.create(number, {
-template: "Your verification code is %token"
-}, function (err, response) {
-if (err) {
-console.log(err)
-res.render('index', {
-error: err.errors[0].description
-})
-}
-else {
-console.log(response)
-res.render('otp', {
-id: response.id
-})
-}
-})
-})
+  messagebird.verify.create(
+    number,
+    {
+      template: "Your verification code is %token",
+    },
+    function (err, response) {
+      if (err) {
+        console.log(err);
+        res.render("index", {
+          error: err.errors[0].description,
+        });
+      } else {
+        console.log(response);
+        res.render("otp", {
+          id: response.id,
+        });
+      }
+    }
+  );
+});
 
-router.post('/step3', (req, res) => {
-var id = req.body.id
-var token = req.body.token
+router.post("/step3", (req, res) => {
+  var id = req.body.id;
+  var token = req.body.token;
 
-messagebird.verify.verify(id, token, (err, response) => {
-if (err) {
-res.send(err)
-}
-else {
-res.render('success')
-}
-})
-})
+  messagebird.verify.verify(id, token, (err, response) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.render("success");
+    }
+  });
+});
 
 module.exports = router;
